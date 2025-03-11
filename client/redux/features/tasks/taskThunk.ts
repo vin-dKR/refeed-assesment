833f235816239bf14fec11c55cf7309329c9e4f8
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-03-11 20:14:33 +0530

    taskThunk done

--- client/redux/features/tasks/taskThunk.ts
+++ client/redux/features/tasks/taskThunk.ts
@@ -18,7 +18,62 @@ export const fetchTasks = createAsyncThunk<Task[]>("tasks/fetchTasks", async (_,
 })
 
 
-// Fetch Task By Id
-// WIP: fetchTaskById-
-// WIP: updateTask-
-// WIP: deleteTask-
+// Fetch Task By fetchTaskById-
+export const fetchTaskById = createAsyncThunk<Task>("tasks/fetchTaskById", async (id, { rejectWithValue }) => {
+    try{
+        const res = await axios.get(`${API_URL}/task/${id}`)
+        return res.data
+    } catch(e) {
+        if (axios.isAxiosError(e)) {
+            return rejectWithValue(e.response?.data?.message || e.message)
+        }
+        return rejectWithValue("an unknown error occured when fetching this task!")
+    }
+})
+
+
+// Add new tasks
+export const addTask = createAsyncThunk<Task, { title: string; description: string; status: string }>(
+    "tasks/addTask", 
+    async (taskData, { rejectWithValue }) => {
+        try {
+            const res = await axios.post(`${API_URL}/task`, taskData)
+            return res.data
+        } catch (e) {
+            if (axios.isAxiosError(e)) {
+                return rejectWithValue(e.response?.data?.message || e.message)
+            }
+            return rejectWithValue("an unknown error occured when adding this task!")
+        }
+    }
+)
+
+// Update Task
+export const updateTask = createAsyncThunk<Task, {id: string; title: string; description: string; status: string}>(
+    "tasks/updateTask", 
+    async ({ id, ...taskData }, { rejectWithValue }) => {
+        try {
+            const res = await axios.put(`${API_URL}/task/${id}`, taskData)
+            return res.data
+        } catch (e) {
+            if (axios.isAxiosError(e)) {
+                return rejectWithValue(e.response?.data?.message || e.message)
+            }
+            return rejectWithValue("an unknown error occured when updating this task!")
+        }
+    }
+)
+
+
+// Delete Task
+export const deleteTask = createAsyncThunk<string, string>("tasks/deleteTask", async (id, { rejectWithValue }) => {
+    try {
+        await axios.delete(`${API_URL}/task/${id}`)
+        return id
+    } catch (e) {
+        if (axios.isAxiosError(e)) {
+            return rejectWithValue(e.response?.data?.message || e.message)
+        }
+        return rejectWithValue("an unknown error occured when deleting this task!")
+    }
+})
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-        'http://localhost:3000',
-        'http://192.168.12.103:3000'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+    app.enableCors({
+        origin: [
+            'http://localhost:3000',
+            'http://192.168.12.103:3000'
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    })
 
-  await app.listen(process.env.PORT ?? 8000);
+    await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
